d56624e98f932fd44030e2dfa1705ce77a31a299
Sonu Babu T <111742766+Sonubabut@users.noreply.github.com>
2024-07-29 16:07:04 +0530

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,7 +6,7 @@ const HomePage = () => {
     <div className="home-page">
       <div className="home-animation">
         {/* Add your animation here */}
-        <img src={"https://test.digitalt3.com/wp-content/uploads/2024/05/land-page_01.png"} alt="Legal AI" className="logo" style={{ width: "300px"}} />
+        <img src={"https://test.digitalt3.com/wp-content/uploads/2024/05/land-page_01.png"} alt="Legal AI" className="logo" style={{ width: "400px", height: "300px"}} />
         <h1>Empowering Legal Professionals with AI!</h1>
         <p>Deliver Legal Solutions with Our AWS and SambaNova Powered Expert</p>
       </div>
