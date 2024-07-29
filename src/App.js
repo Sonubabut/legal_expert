ee877636ab5c3c8f27903257b019dd9e9d694540
Sonu Babu T <111742766+Sonubabut@users.noreply.github.com>
2024-07-29 15:35:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,22 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Chat from './components/Chat/Chat';
 import Sidebar from './components/Sidebar/Sidebar';
 import Home from './components/Home/Home';
-import './App.css';
+import Chat from './components/Chat/Chat';
 
-const App = () => {
-  const [isSidebarOpen, setSidebarOpen] = useState(true);
-
-  const toggleSidebar = () => {
-    setSidebarOpen(!isSidebarOpen);
-  };
+function App() {
 
   return (
     <Router>
-      <div className={`app-container ${isSidebarOpen ? 'sidebar-open' : 'sidebar-closed'}`}>
-        <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
-        <div className="main-content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/chat" element={<Chat />} />
-          </Routes>
-        </div>
+      <Sidebar />
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/chat" element={<Chat />} />
+        </Routes>
       </div>
     </Router>
   );
-};
+}
 
 export default App;
