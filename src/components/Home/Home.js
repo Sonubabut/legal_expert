404286f1efbf402b81aeb297948d489366f3547e
Sonu Babu T <111742766+Sonubabut@users.noreply.github.com>
2024-08-05 13:46:32 +0530

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,6 +9,9 @@ const HomePage = () => {
         <img src={"https://test.digitalt3.com/wp-content/uploads/2024/05/land-page_01.png"} alt="Legal AI" className="logo" style={{ width: "380px", height: "300px"}} />
         <h1>Empowering Legal Professionals with AI!</h1>
         <p>Deliver Legal Solutions with Our AWS and SambaNova Powered Expert</p>
+  <button className="launch-button" onClick={() => window.location.href = "/chat"}>
+          Launch Now!
+        </button>
       </div>
     </div>
   );
